e6426c3c4e5ce18233eb5d95657db70a24206f4d
Zvontsov Sergey <zvoncov@gmail.com>
2023-03-13 13:19:54 +0300

    Create WelcomeView UI

---
+++ Flash_Chat/AppExtension/CustomView.swift
@@ -0,0 +1,36 @@
+//
+//  CustomView.swift
+//  Flash_Chat
+//
+//  Created by Sergey on 13.03.2023.
+//
+
+import UIKit
+
+class CustomView: UIView {
+    
+    override init (frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        layoutViews()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupViews()
+        layoutViews()
+        
+    }
+    
+    func setupViews() {
+        
+    }
+    
+    func layoutViews() {
+        
+    }
+    
+}
---
+++ Flash_Chat/ViewController/WelcomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeViewController.swift
+//  Flash_Chat
+//
+//  Created by Sergey on 13.03.2023.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeViewController: CustomViewController<WelcomeView> {
+    
+    override func loadView() {
+        view = WelcomeView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
